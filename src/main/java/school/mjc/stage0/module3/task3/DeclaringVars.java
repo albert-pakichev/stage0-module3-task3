e6035b04b2a5332d9ab5f042a4cf01f56f8ac85c
albert-pakichev <69592416+albert-pakichev@users.noreply.github.com>
2023-06-25 00:52:09 +0500

    Update DeclaringVars.java

--- src/main/java/school/mjc/stage0/module3/task3/DeclaringVars.java
+++ src/main/java/school/mjc/stage0/module3/task3/DeclaringVars.java
@@ -2,9 +2,7 @@ package school.mjc.stage0.module3.task3;
 
 public class DeclaringVars {
     public static void main(String[] args) {
-        int a = 10;
-        int b = 10;
-        int c = 10;
+        int a,b,c = 10;
         System.out.printf("%d", c);
     }
 }
